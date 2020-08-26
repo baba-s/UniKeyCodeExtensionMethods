fb46201597ae43dcae45adaf43790d9919a0aa6e
baba_s <shotababa1206@gmail.com>
2020-08-26 09:06:56 +0900

    first commit

---
+++ Scripts/KeyCodeExt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Kogane
+{
+	public static class KeyCodeExt
+	{
+		public static bool GetKey( this KeyCode self )
+		{
+			return Input.GetKey( self );
+		}
+
+		public static bool GetKeyDown( this KeyCode self )
+		{
+			return Input.GetKeyDown( self );
+		}
+
+		public static bool GetKeyUp( this KeyCode self )
+		{
+			return Input.GetKeyUp( self );
+		}
+	}
+}
